e70caa57e0153610273bb4c3d01ccb20ac93cb0d
lhp <lhp01678868@alibaba-inc.com>
2023-04-28 18:27:13 +0800

    package-info.java

---
+++ demo-spring/src/main/java/com/pokors/demo/concurrency/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 并发
+ *
+ * @author lhp
+ * @since 2023/4/27 16:10
+ */
+package com.pokors.demo.concurrency;
---
+++ demo-spring/src/main/java/com/pokors/demo/jvm/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Java虚拟机
+ *
+ * @author lhp
+ * @since  2023/4/27 16:00
+ */
+package com.pokors.demo.jvm;
